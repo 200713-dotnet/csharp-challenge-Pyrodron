2ad69f10f7972212a48a06906b3a89daec4b1a03
kyle dick <kyle.i.dick@outlook.com>
2020-07-20 16:54:11 -0400

    first

--- Palindrome.Domain/Class1.cs
+++ Palindrome.Domain/Class1.cs
@@ -2,7 +2,20 @@
 
 namespace Palindrome.Domain
 {
-    public class Class1
-    {
+    public class Class1 {
+      public bool IsPalindrome(string str) {
+        if (str.Length % 2 != 0) {
+          return false;
+        }
+
+        int j = str.Length - 1;
+        for (int i = 0; i < str.Length / 2; i++) {
+          if (str[i] != str[j--]) {
+            return false;
+          }
+        }
+
+        return true;
+      }
     }
 }
--- Palindrome.Testing/UnitTest1.cs
+++ Palindrome.Testing/UnitTest1.cs
@@ -1,14 +1,19 @@
 using System;
 using Xunit;
+using Palindrome.Domain;
 
-namespace Palindrome.Testing
-{
-    public class UnitTest1
-    {
+// dotnet sln add **/*.csproj
+// dotnet add Palindrome.Testing/Palindrome.Testing.csprojk reference Palindrome.Domain/Palindrome.Domain.csproj
+// dotnet run -p Palindrome.Testing/Palindrome.Testing.csproj
+
+namespace Palindrome.Testing {
+    public class UnitTest1 {
+      Class1 pc = new Class1();
+
         [Fact]
-        public void Test1()
-        {
-
+        public void Test1() {
+          pc.IsPalindrome("aaaa");
+          return;
         }
     }
 }
