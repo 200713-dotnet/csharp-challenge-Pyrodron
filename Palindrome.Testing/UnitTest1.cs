c3d8db7872ba377c705ea33d305fc46c8683d30d
kyle dick <kyle.i.dick@outlook.com>
2020-07-20 17:05:17 -0400

    done

--- Palindrome.Testing/UnitTest1.cs
+++ Palindrome.Testing/UnitTest1.cs
@@ -5,6 +5,9 @@ using Palindrome.Domain;
 // dotnet sln add **/*.csproj
 // dotnet add Palindrome.Testing/Palindrome.Testing.csprojk reference Palindrome.Domain/Palindrome.Domain.csproj
 // dotnet run -p Palindrome.Testing/Palindrome.Testing.csproj
+// git add .
+// git commit -m ""
+// git push
 
 namespace Palindrome.Testing {
     public class UnitTest1 {
@@ -12,8 +15,27 @@ namespace Palindrome.Testing {
 
         [Fact]
         public void Test1() {
-          pc.IsPalindrome("aaaa");
-          return;
+          Assert.True(pc.IsPalindrome("aaaa"), "Test1 failed");
+        }
+
+        [Fact]
+        public void Test2() {
+          Assert.True(!pc.IsPalindrome("AaAa"), "Test2 failed");
+        }
+
+        [Fact]
+        public void Test3() {
+          Assert.True(pc.IsPalindrome("AaaA"), "Test3 failed");
+        }
+
+        [Fact]
+        public void Test4() {
+          Assert.True(pc.IsPalindrome("02022020"), "Test4 failed");
+        }
+
+        [Fact]
+        public void Test5() {
+          Assert.True(pc.IsPalindrome("tattarrattat"), "Test5 failed");
         }
     }
 }
